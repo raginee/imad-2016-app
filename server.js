7a83eaba81ca8ecad6e00bfc4b27b0632b299cec
raginee <priyachaturvedi25@gmail.com>
2016-09-20 22:26:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get('/secondarticle',function(req,res){
     res.sendFile(path.join(__dirname,'ui','secondarticle.html'));
 }
 );
+app.get('/thirdarticle',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','thirdarticle.html'));
+}
+);
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
